e5c899958e2f094652815abca0dacfd93826db16
Gergő Móricz <mo.geryy@gmail.com>
2017-06-20 14:06:48 +0200

    v0, API only.

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # twitter-whois
 A Twitter whois service: see where are people blocked, and why are they blocked in those countries.
+
+# API
+
+## /scan/username
+
+GET whois.skiilaa.me/scan/username
+
+Example:
+
+```
+whois.skiilaa.me/scan/skiilaa
+```
+
+Response:
+
+```
+User is not restricted in any country.
+User is not suspended.
+```
+
+# How to setup on your own
+
+## Create Twitter app.
+
+Go to [your Twitter apps](https://apps.twitter.com), create an app, and copy in you consumer and access keys into `config.json` like this:
+
+```json
+{
+    "consumerkey": "Your Consumer Key",
+    "consumersecret": "Your Consumer Secret",
+    "accesskey": "Your Access Token",
+    "accsec": "Your Access Secret"
+}
+```
+
+## Run this script.
+
+Run `npm install`, run `node index.js`, and boom. Visit localhost:5374/scan/skiilaa, and there it is.
---
+++ index.js
@@ -0,0 +1,72 @@
+/*
+ * twitter-whois
+ * Check where/why Twitter users are blocked.
+ * Made by skiilaa
+ * Licensed under GPL-3.0
+ */
+
+//Required packages
+var express = require('express');
+var twitter = require('twitter');
+var country = require('country-data').countries;
+var lookup = require('country-data').lookup;
+
+//Config file
+var config = require('./config.json');
+
+//Package setup
+var app = express();
+var client = new twitter({
+    consumer_key: config.consumerkey,
+    consumer_secret: config.consumersecret,
+    access_token_key: config.accesskey,
+    access_token_secret: config.accsec
+});
+
+//Express app
+app.get("/scan/:username", function(req, res){
+    console.log("Scanning for " + req.params.username);
+    var result = "";
+    client.get("users/show", { screen_name: req.params.username }, function(error, user){
+        if (error){
+            var reported = false;
+            error.forEach(function(element){
+                if (typeof element.code != "undefined"){
+                    if (element.code == 63){
+                        result = "User is suspended.";
+                        reported = true;
+                    }
+                }
+            });
+            if (!reported){
+                 result = "An error occurred.";
+            }
+        } else {
+            if (typeof user.withheld_in_countries != "undefined") {
+                var wcount = 0;
+                withheld_in_countries.forEach(function(element) {
+                    wcount++;
+                    var lookupResult = lookup.countries({code: element});
+                    if (lookupResult.length > 0){
+                        result += "User is restricted in " + lookupResult[0].name + "<br>";
+                    } else {
+                        result += "Twitter returned ~weird~ data.<br>";
+                    }
+                });
+                if (wcount == 0){
+                    result += "User is not restricted in any country.<br>";
+                }
+            } else {
+                result += "User is not restricted in any country.<br>";
+            }
+            if (user.suspended){
+                result += "User is suspended.";
+            } else {
+                result += "User is not suspended.";
+            }
+        }
+        res.send(result);
+    });
+});
+
+app.listen(5374);
